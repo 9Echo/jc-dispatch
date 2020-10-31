959470c8f0946145ba8b17352d764f0752ef38b6
9Echo <1094015147@qq.com>
2020-10-31 09:18:52 +0800

    更新

--- MultiObjectiveDispatch/services/Generate_candidate_set.py
+++ MultiObjectiveDispatch/services/Generate_candidate_set.py
@@ -2,7 +2,8 @@
 # 日期： 2020/9/15
 # 时间： 14:56
 import pandas as pd
-from dispatch1.entity import LoadTask
+from pandas.core.frame import DataFrame
+from MultiObjectiveDispatch.entity import LoadTask
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -49,12 +50,11 @@ def deal_stock(data):
 
 def generate_candidate(stock_data, truck):
     """
-
     :param stock_data:
     :param truck:
     :return:
     load_task_candidate = {key:value}
-    key: driver_id
+    key: car_mark
     value:[[list]]
     """
 
@@ -66,18 +66,16 @@ def generate_candidate(stock_data, truck):
 
     n = len(truck)
     # 遍历车次列表
-    for i in range(n):
+    for i in range(0, 1):
 
         # 一辆车所有的装车清单候选集
         candidate_set = []
         # 一份打包好的货物list
         candidate_for_one_truck = []
         # 初始化字典
-        load_task_candidate[truck.loc[i]['driver_id']] = candidate_set
+        load_task_candidate[truck.loc[i]['car_mark']] = candidate_set
         # 取出和当前车次的城市和品名相同的货物
         stock_list_city_commodity = stock_data[(stock_data['城市'] == truck.loc[i]['city']) & (stock_data['品名'] == truck.loc[i]['big_commodity_name'])]
-        # 取出已分类货物的index方便定位，此index即为当前类别下货物行数
-        index = stock_list_city_commodity.index
         # 取出可发重量
         can_be_sent_weight = stock_list_city_commodity['actual_weight'].tolist()
         # 取出单件重量
@@ -93,62 +91,74 @@ def generate_candidate(stock_data, truck):
         # 得到最大单个候选集长度
         longest_iteration = weight_up//min_weight
 
+        # print(stock_list_city_commodity)
         # 通过候选集长度进行遍历
-        for long_iter in range(longest_iteration):
-            # 当长度为1时，只需遍历一次货物列表即可
-            if long_iter == 0:
-                for j in range(len(index)):
-                    candidate_for_one_truck = []
-                    current_weight = stock_list_city_commodity.loc[index[j]]['unit_weight']
-                    if weight_up >= current_weight >= weight_down:
-                        candidate_for_one_truck.append(index[j])
-                        candidate_for_one_truck.append(current_weight)
-                        candidate_set.append(candidate_for_one_truck)
-            else:
-                # 遍历两次当前已分类货物列表
-                for j in range(len(index)):
-                    for j_temp in range(j+1, len(index)):
-                        candidate_for_one_truck = []
-                        sum_weight = 0
-                        while len(candidate_for_one_truck) <= long_iter+1:
-                            if sum_weight + stock_list_city_commodity.loc[index[j]]['unit_weight'] <= weight_up:
-                                candidate_for_one_truck.append(index[j])
-                                sum_weight += stock_list_city_commodity.loc[index[j]]['unit_weight']
 
-                        if sum_weight >= weight_down:
-                            candidate_for_one_truck.append(sum_weight)
-                            candidate_set.append(candidate_for_one_truck)
-
-
-        # 开始生成装车清单集合
-
-        # 遍历当前已分类货物列表
-
-            if sum_weight <= weight_up:
-                candidate_for_one_truck.append(index[j])
-                sum_weight += stock_list_city_commodity.loc[index[j]]['unit_weight']
-            elif sum_weight > weight_up:
-                sum_weight = 0
-                candidate_for_one_truck = []
+        # 根据重量规则生成候选集
+        candidate_for_one_truck = dfs_candidate(stock_list_city_commodity, weight_up, weight_down)
 
         # 将当前车次的候选集作为 value 存入字典
-        load_task_candidate[truck.loc[i]['driver_id']] = candidate_set
+        load_task_candidate[truck.loc[i]['car_mark']] = candidate_set
 
         # print(load_task_candidate)
     return load_task_candidate
 
 
-def rank(load_task_candidate):
+def dfs_candidate(weight_list, weight_up, weight_down):
+    """
+    深度搜索实现枚举过程
+    :param weight_list: 当前经过筛选的库存数据
+    :param weight_up: 当前车次载重上限
+    :param weight_down: 当前车次载重下限
+    :return candidate_set: 当前车次所有可装载的预装车清单
+    """
+    if weight_list.empty:
+        return None
 
-    load_task_rank = load_task_candidate
+    candidate_set = []
+
+    def dfs(l1, l, r):
+        """
+        枚举
+        :param l1: 当前满足重量要求的临时dataframe
+        :param l: 左指针
+        :param r: 右指针
+        :return:
+        """
+        n = l1.weight.sum()
+        # print(weight_up, weight_down, n)
+        if weight_down <= n <= weight_up:
+            l1_list = list(l1.index_weight)
+            candidate_set.append(l1_list)
+            # print(candidate_set)
+        elif n > weight_up or l > r:
+            return
+        for j in range(0, r-l+1):
+            l1_index = weight_l1.loc[l+j]['index_weight']
+            l1_temp = weight_l1[weight_l1['index_weight'] == l1_index]
+            dfs(l1.append(l1_temp, ignore_index=True), l+j+1, r-1)
+
+    # 取出已分类货物的index方便定位，此index即为当前类别下货物index
+    index = weight_list.index
+    index_weight = list(index)
+    weight = list(weight_list['unit_weight'])
+    weight_temp = {'index_weight': index_weight, 'weight': weight}
+    # weight_l1只包含原dataframe的货物下标和对应的货物重量
+    weight_l1 = DataFrame(weight_temp)
+
+    for i in range(len(index)):
+        dfs(weight_l1[weight_l1['index_weight'] == index[i]], 1, len(index)-1)
+
+    for ind in range(len(candidate_set)):
+        print(candidate_set[ind], list(weight_l1[weight_l1['index_weight'] == candidate_set[ind][0]]['weight']))
+    # print(candidate_set)
+    return candidate_set
 
 
-    return load_task_rank
+def dispatch(load_task_candidate, truck):
 
-
-def dispatch(load_task_rank, truck):
-
-    load_task = LoadTask()
+    # load_task = LoadTask()
+    load_task = []
 
     return load_task
 
@@ -156,19 +166,16 @@ def dispatch(load_task_rank, truck):
 if __name__ == "__main__":
 
     # 输入当天库存数据
-    stock_day = pd.read_excel("./test_stock.xls")
+    stock_day = pd.read_excel(r"D:\Users\pc\PycharmProjects\jc-dispatch\MultiObjectiveDispatch\test_stock.xls")
 
     # 库存数据预处理
     stock_data = deal_stock(stock_day)
 
     # 车辆数据（非真实数据）
-    truck = pd.read_excel("./truck.xls")
+    truck = pd.read_excel("D://Users//pc//PycharmProjects//jc-dispatch//MultiObjectiveDispatch//truck.xls")
 
     # 根据库存为当前车次（30辆车）的每一辆车生成装车清单候选集
-    load_task_candidate = generate_candidate(stock_data, truck)
-
-    # 利用rank函数为装车清单候选集排序
-    load_task_rank = rank(load_task_candidate)
+    load_task_candidates = generate_candidate(stock_data, truck)
 
     # 优先将rank值高的候选集与车次进行匹配
-    load_task_dispatch = dispatch(load_task_rank, truck)
+    load_task_dispatch = dispatch(load_task_candidates, truck)
